caca883817efb46b21640ce22de59760d3e78984
Algorytmouse <ceylan.cemil99@googlemail.com>
2025-01-04 16:32:00 +0100

    Dreieck generieren

--- Dreieck/src/Main.java
+++ Dreieck/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int h = 8;
+        char block = '*';
+
+        for (int i = 1; i<h+1; i++){
+            for (int j = 1; j<=i; j++){
+                System.out.print(block);
+            }
+            System.out.println();
+        }
     }
 }
